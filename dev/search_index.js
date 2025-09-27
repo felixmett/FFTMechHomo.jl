ecc24a3fca5b658b73963d7b5d8824daa9afc65c
Documenter.jl <documenter@juliadocs.github.io>
2025-09-27 18:43:38 +0000

    build based on e7cb7a0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FFTMechHomo","page":"Home","title":"FFTMechHomo","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FFTMechHomo.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
