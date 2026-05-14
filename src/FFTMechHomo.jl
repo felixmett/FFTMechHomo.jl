cab45e80a7c2b1a1c6076501c669346139319be6
felixmett <felixmett@googlemail.com>
2026-05-14 13:25:32 +0200

    Move abstract material type to seperate file

--- src/FFTMechHomo.jl
+++ src/FFTMechHomo.jl
@@ -1,25 +1,9 @@
 module FFTMechHomo
 
-"""
-    AbstractMaterial
-
-Abstract supertype for all material models. 
-
-# Voigt Convention
-All materials use engineering shear strains (γ = 2ε):
-
-    strain = [ε₁₁, ε₂₂, (ε₃₃,) γ₁₂, (γ₁₃, γ₂₃)]
-    stress = [σ₁₁, σ₂₂, (σ₃₃,) σ₁₂, (σ₁₃, σ₂₃)]
-
-# Interface
-Every material must implement:
-- `compute_stress!(stress, strain, material, i)` 
-"""
-abstract type AbstractMaterial end
-
 # structs
 export LinearIsotropicElastic
 
+include("materials/material.jl")
 include("materials/elastic.jl")
 
 end
