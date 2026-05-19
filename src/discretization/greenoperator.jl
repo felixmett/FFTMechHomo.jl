31e902a8d82b727256b213a410177410e9d93af3
felixmett <felixmett@googlemail.com>
2026-05-19 15:53:25 +0200

    Add voigt index utility function for matrix index mapping

--- src/discretization/greenoperator.jl
+++ src/discretization/greenoperator.jl
@@ -9,3 +9,25 @@ function is_first_or_nyquist(idx::CartesianIndex, disc::AbstractDiscreteGreenOpe
     end
     return false
 end
+
+"""
+    voigt_index(::Val{2})
+    voigt_index(::Val{3})
+
+Return the Voigt index mapping for a symmetric matrix of dimension `d`.
+
+The returned nested tuple `vidx` satisfies `vidx[i][j] == v` where `v` is the
+index into the Voigt vector corresponding to the `(i, j)` entry of a symmetric
+matrix. See [`AbstractMaterial`](@ref) for the Voigt convention used throughout.
+
+# Returns
+- `NTuple{d, NTuple{d, Int}}`: Nested tuple representing the `d×d` index matrix
+
+# Example
+```julia
+vidx = voigt_index(Val(2))
+vidx[1][2]  # → 3, i.e. the (1,2) matrix entry is stored at Voigt index 3
+```
+"""
+voigt_index(::Val{2}) = ((1, 3), (3, 2))
+voigt_index(::Val{3}) = ((1, 6, 5), (6, 2, 4), (5, 4, 3))
