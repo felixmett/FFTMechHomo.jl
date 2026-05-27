21b8be3d238f0155c5f995c0a689dbc4779ebd00
felixmett <felixmett@googlemail.com>
2026-05-27 14:25:18 +0200

    Add utility functions for fft plans and cell averages in fourier space

---
+++ src/utilities/fft.jl
@@ -0,0 +1,19 @@
+function make_fft_plans(
+    solver::AbstractSolver,
+    field::AbstractArray{T},
+    dim::Int
+) where {T}
+    FFTW.set_num_threads(solver.FFTW_num_threads)
+    fft_plan  = plan_rfft(field, 2:dim+1; flags=solver.FFTW_flags)
+    ifft_plan = plan_irfft(field, size(field, 2), 2:dim+1; flags=solver.FFTW_flags)
+    return fft_plan, ifft_plan
+end
+
+function cell_average!(
+    cell_avg::Vector{T}, 
+    field_fft::AbstractArray{Complex{T}}, 
+    consts::GridConstants{dim, T}
+) where {dim, T}
+    cell_avg .= real(field_fft[1:consts.n_voigt, consts.zero_idx]) ./ consts.n_cells
+    return
+end
