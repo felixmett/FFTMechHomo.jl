b31a91eb5ed80142410335a02f241d48888549bb
felixmett <felixmett@googlemail.com>
2026-05-27 14:24:16 +0200

    Add function for residual computation

--- src/solver/solver.jl
+++ src/solver/solver.jl
@@ -17,6 +17,19 @@ function GridConstants(microstructure::InternalMicrostructure{dim, T}) where {di
    return GridConstants{dim, T}(n_voigt, n_cells, zero_idx)
 end
 
+function residual(
+    stress::AbstractArray{T},
+    strain::AbstractArray{T},
+    strain_prev::AbstractArray{T},
+    stress_avg::AbstractVector{T},
+    ref::ReferenceMaterial{dim, T},
+    consts::GridConstants{dim, T}
+) where {dim, T}
+    stress .= strain .- strain_prev
+    compute_stress_field!(stress, stress, ref)
+    stress_mean = dropdims(mean(stress, dims=2:dim+1), dims=Tuple(2:dim+1))
+    return sqrt((T(1) / consts.n_cells) * tensor_dot(stress_mean, :stress) / tensor_dot(stress_avg, :stress))
+end
 
 # # Somehow make this aware if linear or not
 # struct Solution{S <: AbstractSolver}
