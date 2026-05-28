c69e511a654b5b4005fda702a016ca38710ee29e
felixmett <felixmett@googlemail.com>
2026-05-28 23:44:26 +0200

    Add documentation for Microstructure constructor

--- src/microstructure/microstructure.jl
+++ src/microstructure/microstructure.jl
@@ -2,6 +2,23 @@ struct Microstructure{dim, T <: AbstractFloat, A <: AbstractArray{<:AbstractMate
     materials::A       
 end
 
+"""
+    Microstructure(materials)
+
+Construct a microstructure from a spatial array of material models.
+
+Each element of `materials` corresponds to one voxel of the microstructure.
+All materials must have consistent spatial dimension and numeric type.
+
+# Arguments
+- `materials`: array of [`AbstractMaterial`](@ref) instances with shape `(n₁, n₂)` for 2D or `(n₁, n₂, n₃)` for 3D
+
+# Example
+```julia
+mat = LinearIsotropicElastic{3}(210e3, 0.3)
+ms  = Microstructure(fill(mat, 32, 32, 32))
+```
+"""
 function Microstructure(materials::AbstractArray{<:AbstractMaterial})
     dim = ndims(first(materials))
     T = eltype(first(materials))
@@ -24,5 +41,5 @@ function Microstructure(materials::AbstractArray{<:AbstractMaterial})
     Microstructure{dim, T, typeof(materials)}(materials)
 end
 
-Base.ndims(::Microstructure{dim, T, A}) where {dim, T <: AbstractFloat, A <: AbstractArray{<:AbstractMaterial}} = d
+Base.ndims(::Microstructure{dim, T, A}) where {dim, T <: AbstractFloat, A <: AbstractArray{<:AbstractMaterial}} = dim
 Base.size(microstructure::Microstructure{dim, T, A}) where {dim, T <: AbstractFloat, A <: AbstractArray{<:AbstractMaterial}} = size(microstructure.materials)
