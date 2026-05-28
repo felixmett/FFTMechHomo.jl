5fd503001c37c4c7160efc6df1781dd7390af42b
felixmett <felixmett@googlemail.com>
2026-05-28 23:42:17 +0200

    Refactor documentation of material interface

--- src/material/material.jl
+++ src/material/material.jl
@@ -9,11 +9,35 @@ All materials use engineering shear strains (γ = 2ε):
     strain = [ε₁₁, ε₂₂, (ε₃₃,) γ₁₂, (γ₁₃, γ₂₃)]
     stress = [σ₁₁, σ₂₂, (σ₃₃,) σ₁₂, (σ₁₃, σ₂₃)]
 
-# Interface
-Every material must implement:
-- `compute_stress!(stress, strain, material, i)`
+Subtypes are categorized by memory behaviour:
+- [`HistoryIndependent`](@ref): stress depends only on current strain
+- [`HistoryDependent`](@ref): stress depends on strain history and internal variables
 """
 abstract type AbstractMaterial end
-# TODO: Document that
+
+"""
+    HistoryIndependent <: AbstractMaterial
+
+Abstract supertype for materials whose stress response depends only on the
+current strain, with no internal variables. Example: linear elasticity
+"""
 abstract type HistoryIndependent <: AbstractMaterial end
+
+"""
+    HistoryDependent <: AbstractMaterial
+
+Abstract supertype for materials with internal variables that evolve over the
+deformation history. Examples: plasticity, damage, viscoelasticity.
+"""
 abstract type HistoryDependent <: AbstractMaterial end
+
+"""
+    compute_stress!(stress, strain, material::AbstractMaterial, i::CartesianIndex)
+
+Compute the stress response of `material` at grid index `i` in-place.
+
+# Interface
+Subtypes of [`AbstractMaterial`](@ref) must implement this method. The stress
+and strain arrays follow the Voigt convention defined in [`AbstractMaterial`](@ref).
+"""
+function compute_stress! end
