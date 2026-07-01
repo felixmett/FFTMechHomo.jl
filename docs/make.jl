d5f1398803ea4379e7f0e3a147e61814462c80b5
felixmett <felixmett@googlemail.com>
2026-07-01 22:30:03 +0200

    Add julia script to make docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,50 @@
-using FFTMechHomo
 using Documenter
+using DocumenterCitations
+using FFTMechHomo
 
-DocMeta.setdocmeta!(FFTMechHomo, :DocTestSetup, :(using FFTMechHomo); recursive=true)
-
-makedocs(;
-    modules=[FFTMechHomo],
-    authors="Felix Mett",
-    sitename="FFTMechHomo.jl",
-    format=Documenter.HTML(;
-        canonical="https://Cr0gan.github.io/FFTMechHomo.jl",
-        edit_link="master",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-    ],
+DocMeta.setdocmeta!(
+    FFTMechHomo,
+    :DocTestSetup,
+    :(using FFTMechHomo)
 )
 
-deploydocs(;
-    repo="github.com/Cr0gan/FFTMechHomo.jl",
-    devbranch="master",
+bib = CitationBibliography(joinpath(@__DIR__, "references.bib"))
+
+makedocs(
+    modules = [FFTMechHomo],
+    plugins = [bib],
+    format = Documenter.HTML(
+        prettyurls = get(ENV, "CI", nothing) == "true",
+        canonical = "https://felixmett.github.io/FFTMechHomo.jl"
+    ),
+    sitename = "FFTMechHomo.jl",
+    authors = "Felix Mett",
+    pages = [
+        "Home" => "index.md",
+        "Background" => "background.md",
+        "Examples" => [
+			"Custom Material Definition" => "examples/custommaterial.md",
+			"Effective Elasticity Tensor" => "examples/effectivetensor.md",
+		],
+        "API" => [
+            "Overview" => "api/index.md",
+            "Discretization" => "api/discretization.md",
+            "Microstructure" => "api/microstructure.md",
+            "Materials" => "api/material.md",
+            "Solver" => "api/solver.md",
+        ],
+        "References" => "references.md",
+    ],
+    warnonly = false,
+    draft = false,
+    source = "src",
+    build = "build",
+)
+
+deploydocs(
+    repo = "github.com/felixmett/FFTMechHomo.jl",
+    target = joinpath(@__DIR__, "build"),
+    branch = "gh-pages",
+    devbranch = "master",
+    push_preview = true,
 )
