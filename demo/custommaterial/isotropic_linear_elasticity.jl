733c1e92ad89d79ee89af0eec85686540800185e
felixmett <felixmett@googlemail.com>
2026-07-01 16:53:27 +0200

    Add demo scripts generated from Literate

---
+++ demo/custommaterial/isotropic_linear_elasticity.jl
@@ -0,0 +1,49 @@
+using FFTMechHomo
+
+struct CustomElasticity{dim, T <: AbstractFloat} <: HistoryIndependent{dim, T}
+    E::T
+    nu::T
+    μ::T
+    λ::T
+    function CustomElasticity{dim, T}(E::T, nu::T) where {dim, T <: AbstractFloat}
+        μ = E / (2 * (1 + nu))
+        λ = E * nu / ((1 + nu) * (1 - 2nu))
+        new{dim, T}(E, nu, μ, λ)
+    end
+end
+
+function FFTMechHomo.compute_stress!(
+    stress::AbstractArray{T},
+    strain::AbstractArray{T},
+    mat::CustomElasticity{dim, T},
+    i::CartesianIndex
+) where {dim, T <: AbstractFloat}
+    tr_strain = sum(strain[1:dim, i])
+    stress[1:dim, i] .= 2mat.μ .* strain[1:dim, i] .+ mat.λ * tr_strain
+    stress[dim+1:end, i] .= mat.μ .* strain[dim+1:end, i]
+    return
+end
+
+simple_microstructure(mat1, mat2) = Microstructure([i==1 ? mat1 : mat2 for i in 1:3, j in 1:3, k in 1:3])
+
+dim = 3
+mat_soft_custom = CustomElasticity{dim, Float64}(2100., 0.3)
+mat_hard_custom = CustomElasticity{dim, Float64}(72000., 0.22)
+microstructure = simple_microstructure(mat_soft_custom, mat_hard_custom)
+
+disc = MoulinetSuquetDiscretization(microstructure)
+macro_strain = MacroscopicStrain([0.01, 0, 0, 0, 0, 0])
+solver = BasicScheme(mat_soft_custom.μ + mat_hard_custom.μ, microstructure)
+sol = solve(microstructure, disc, macro_strain, solver)
+
+mat_soft_builtin = LinearIsotropicElastic{dim, Float64}(2100., 0.3)
+mat_hard_builtin = LinearIsotropicElastic{dim, Float64}(72000., 0.22)
+validation_microstructure = simple_microstructure(mat_soft_builtin, mat_hard_builtin)
+
+disc = MoulinetSuquetDiscretization(validation_microstructure)
+solver = BasicScheme(mat_soft_builtin.μ + mat_hard_builtin.μ, microstructure)
+validation_sol = solve(microstructure, disc, macro_strain, solver)
+
+@assert sol.stress_avg ≈ validation_sol.stress_avg
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
---
+++ demo/effectivetensor/effective_elasticity_tensor.jl
@@ -0,0 +1,49 @@
+using FFTMechHomo
+using LinearAlgebra
+using Plots
+
+dim = 2
+L = 257
+r = 64
+centered_range(L::Int) = collect(1:L) .- (L÷2 + mod(L,2))
+
+X = centered_range(L)
+indicator = [x^2 + y^2 <= r^2 ? 1 : 2 for x in X, y in X]
+
+heatmap(
+    indicator;
+    color=:jet, colorbar=false,
+    xticks=[1, 64, 128, 192, 256],
+    yticks=[1, 64, 128, 192, 256],
+    xlabel="X", ylabel="Y"
+)
+
+mat1 = LinearIsotropicElastic{dim}(72000., 0.22)
+mat2 = LinearIsotropicElastic{dim}(48000., 0.3)
+microstructure = Microstructure(map(x -> x == 1 ? mat1 : mat2, indicator))
+
+disc = MoulinetSuquetDiscretization(microstructure)
+α₀ = 1.5(mat1.μ + mat2.μ)
+solver = BasicScheme(α₀, microstructure)
+
+ε0 = 0.01
+macro_strains = [
+    MacroscopicStrain([ε0, 0, 0]),
+    MacroscopicStrain([0, ε0, 0]),
+    MacroscopicStrain([0, 0, ε0])
+]
+
+ℂ = zeros(3,3)
+for (i, macro_strain) in enumerate(macro_strains)
+    sol = solve(microstructure, disc, macro_strain, solver)
+    @assert sol.converged "Solver has not converged!"
+
+    ℂ[i,:] = sol.stress_avg ./ ε0
+end
+
+test_strain = MacroscopicStrain([ε0, ε0, ε0])
+test_sol = solve(microstructure, disc, test_strain, solver)
+homogenized_stress = ℂ * test_strain.data
+@assert isapprox(test_sol.stress_avg, homogenized_stress; rtol=1e-3)
+
+# This file was generated using Literate.jl, https://github.com/fredrikekre/Literate.jl
