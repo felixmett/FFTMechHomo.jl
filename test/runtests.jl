ce04401ad2a1a0336a588b706d5847bada10556f
felixmett <felixmett@googlemail.com>
2026-07-02 16:44:34 +0200

    Add all tests to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,4 +3,19 @@ using FFTW
 using LinearAlgebra
 using Test
 
-include("materials/elastic.jl")
+include("discretization/greenoperator.jl")
+include("discretization/moulinetsuquet.jl")
+
+include("material/models/elastic.jl")
+include("material/models/reference.jl")
+
+include("microstructure/internal.jl")
+include("microstructure/microstructure.jl")
+
+include("solver/basic.jl")
+include("solver/macroscopicstrain.jl")
+include("solver/solver.jl")
+
+include("utilities/tensors.jl")
+
+include("integration.jl")
